2c6e4570463c2de6a57960d23dbded18e4ba0f6a
madyy-digital <mardiathmama44@gmail.com>
2026-08-30 21:08:39 +0200

    Delete page.jsx

--- page.jsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import React, { useState, useEffect } from 'react';
-import { Menu, X, ArrowRight, CheckCircle, Send, Star } from 'lucide-react';
-
-export default function MadyyDigital() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  ...
